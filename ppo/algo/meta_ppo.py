7732e0ef58801a4963a2d93669d67b4257c378dd
albertbou92 <albertbo@kth.se>
2021-04-07 11:47:03 +0200

    meta ppo

---
+++ ppo/algo/meta_ppo.py
@@ -0,0 +1,292 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from shutil import copy2
+from copy import deepcopy
+
+
+class MetaPPO():
+    def __init__(self,
+                 actor_critic,
+                 clip_param,
+                 ppo_epoch,
+                 num_mini_batch,
+                 value_loss_coef,
+                 entropy_coef,
+                 lr=None,
+                 eps=None,
+                 max_grad_norm=None,
+                 use_clipped_value_loss=True):
+
+        self.actor_critic = actor_critic
+
+        self.clip_param = clip_param
+        self.ppo_epoch = ppo_epoch
+        self.num_mini_batch = num_mini_batch
+
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
+
+        self.optimizer = optim.Adam(actor_critic.policy.parameters(), lr=lr, eps=eps)
+        self.meta_optimizer = optim.Adam(actor_critic.intrinsic_reward_net.parameters(), lr=lr, eps=eps)
+
+    def compute_loss(self, action_logp, old_action_logp, adv, values, old_values, returns, dist_entropy):
+
+        ratio = torch.exp(action_logp - old_action_logp)
+        surr1 = ratio * adv
+        surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv
+        action_loss = - torch.min(surr1, surr2).mean()
+
+        if self.use_clipped_value_loss:
+            value_pred_clipped = old_values + (values - old_values).clamp(-self.clip_param, self.clip_param)
+            value_losses = (values - returns).pow(2)
+            value_losses_clipped = (value_pred_clipped - returns).pow(2)
+            value_loss = 0.5 * torch.max(value_losses, value_losses_clipped).mean()
+        else:
+            value_loss = 0.5 * (returns - values).pow(2).mean()
+
+        loss = value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef
+
+        return loss, value_loss, action_loss
+
+    def update(self, rollouts):
+
+        value_loss_epoch = 0
+        action_loss_epoch = 0
+        dist_entropy_epoch = 0
+
+        for e in range(self.ppo_epoch):
+
+            if self.actor_critic.is_recurrent:
+                data_generator = rollouts.recurrent_generator(self.num_mini_batch)
+            else:
+                data_generator = rollouts.feed_forward_generator(self.num_mini_batch)
+
+            for sample in data_generator:
+
+                obs_batch, recurrent_hidden_states_batch, actions_batch, \
+                value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
+                adv_targ = sample
+
+                # Generate intrinsic rewards
+                int_rewards, _ = self.actor_critic.predict_intrinsic(obs_batch, actions_batch)
+                mix_return_batch = return_batch + int_rewards
+
+                ###############################################################
+
+                # Compute  mixed advantage
+                adv_targ = mix_return_batch - value_preds_batch
+                adv_targ = (adv_targ - adv_targ.mean()) / (adv_targ.std() + 1e-5)
+
+                # Reshape to do in a single forward pass for all steps
+                values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
+                    obs_batch, recurrent_hidden_states_batch, masks_batch, actions_batch)
+
+                # Compute normal loss
+                ratio = torch.exp(action_log_probs - old_action_log_probs_batch)
+                surr1 = ratio * adv_targ
+                surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
+                action_loss = - torch.min(surr1, surr2).mean()
+
+                if self.use_clipped_value_loss:
+                    value_pred_clipped = value_preds_batch + (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
+                    value_losses = (values - mix_return_batch).pow(2)
+                    value_losses_clipped = (value_pred_clipped - mix_return_batch).pow(2)
+                    value_loss = 0.5 * torch.max(value_losses, value_losses_clipped).mean()
+                else:
+                    value_loss = 0.5 * (mix_return_batch - values).pow(2).mean()
+
+                loss = value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef
+
+                # Clean grads from previous iteration in both optimizers
+                self.optimizer.zero_grad()
+                self.meta_optimizer.zero_grad()
+
+                # Normal backward pass
+                loss.backward()
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+
+                value_loss_epoch += value_loss.item()
+                action_loss_epoch += action_loss.item()
+                dist_entropy_epoch += dist_entropy.item()
+
+                ###############################################################
+
+                adv_targ = return_batch - (value_preds_batch - int_rewards.detach())
+                adv_targ = (adv_targ - adv_targ.mean()) / (adv_targ.std() + 1e-5)
+
+                # Reshape to do in a single forward pass for all steps
+                values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
+                    obs_batch, recurrent_hidden_states_batch, masks_batch, actions_batch)
+
+                # Compute meta loss
+                ratio = torch.exp(action_log_probs - old_action_log_probs_batch)
+                surr1 = ratio * adv_targ
+                surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
+                action_loss = - torch.min(surr1, surr2).mean()
+
+                values -= int_rewards.detach()
+                value_preds_batch -= int_rewards.detach()
+                if self.use_clipped_value_loss:
+                    value_pred_clipped = value_preds_batch + (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
+                    value_losses = (values - return_batch).pow(2)
+                    value_losses_clipped = (value_pred_clipped - return_batch).pow(2)
+                    value_loss = 0.5 * torch.max(value_losses, value_losses_clipped).mean()
+                else:
+                    value_loss = 0.5 * (return_batch - values).pow(2).mean()
+
+                meta_loss = value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef
+
+                # Meta backward pass
+                meta_loss.backward()
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+                self.meta_optimizer.step()
+
+        num_updates = self.ppo_epoch * self.num_mini_batch
+
+        value_loss_epoch /= num_updates
+        action_loss_epoch /= num_updates
+        dist_entropy_epoch /= num_updates
+
+        return value_loss_epoch, action_loss_epoch, dist_entropy_epoch
+
+
+    def sample_update(self, sample):
+
+        # this is the computation of the ppo loss, which is common to both the in_sample and ex_sample
+
+        obs_batch, recurrent_hidden_states_batch, actions_batch, \
+            value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
+                adv_targ = sample
+
+        # Reshape to do in a single forward pass for all steps
+        values, action_log_probs, dist_entropy, _, dist_a = self.actor_critic.evaluate_actions(
+            obs_batch, recurrent_hidden_states_batch, masks_batch,actions_batch)
+
+        ratio = torch.exp(action_log_probs - old_action_log_probs_batch)
+        surr1 = ratio * adv_targ
+        surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
+        action_loss = -torch.min(surr1, surr2).mean()
+
+        if self.use_clipped_value_loss:
+            value_pred_clipped = value_preds_batch + \
+                (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
+            value_losses = (values - return_batch).pow(2)
+            value_losses_clipped = (
+                value_pred_clipped - return_batch).pow(2)
+            value_loss = 0.5 * torch.max(value_losses, value_losses_clipped).mean()
+        else:
+            value_loss = 0.5 * (return_batch - values).pow(2).mean()
+
+        loss = value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef
+
+        loss.backward(retain_graph=True)
+        nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+
+        return value_loss, action_loss, dist_entropy
+
+    def update(self, in_rollouts, ex_rollouts):
+        # Advantages are computed for both rollouts and are kept separate
+        in_advantages = in_rollouts.returns[:-1] - in_rollouts.value_preds[:-1]
+        in_advantages = (in_advantages - in_advantages.mean()) / (in_advantages.std() + 1e-5)
+
+        ex_advantages = ex_rollouts.returns[:-1] - ex_rollouts.value_preds[:-1]
+        ex_advantages = (ex_advantages - ex_advantages.mean()) / (
+            ex_advantages.std() + 1e-5)
+
+        value_loss_epoch = 0
+        action_loss_epoch = 0
+        dist_entropy_epoch = 0
+        kl_div_epoch = 0
+
+        for e in range(self.ppo_epoch):
+            # From the 2 advantages 2 generators are created from which we sample simultaneusly ex_sample and in_sample
+            if self.actor_critic.is_recurrent:
+                data_generator_in = in_rollouts.recurrent_generator(in_advantages, self.num_mini_batch)
+            else:
+                data_generator_in = in_rollouts.feed_forward_generator(in_advantages, self.num_mini_batch)
+
+            if self.actor_critic.is_recurrent:
+                data_generator_ex = ex_rollouts.recurrent_generator(ex_advantages, self.num_mini_batch)
+            else:
+                data_generator_ex = ex_rollouts.feed_forward_generator(ex_advantages, self.num_mini_batch)
+
+            for ex_sample, in_sample in zip(data_generator_ex, data_generator_in):
+
+                self.optimizer.zero_grad()
+                self.meta_optimizer.zero_grad()
+
+                # compute loss first for intrinsic reward then external reward update of eta with meta_optimizer
+                value_loss, action_loss, dist_entropy, kl_div = self.sample_update(in_sample)
+                self.optimizer.step()
+
+                value_loss, action_loss, dist_entropy, kl_div = self.sample_update(ex_sample)
+                self.meta_optimizer.step()
+                #para_tensor = list(self.rew_fun.parameters())
+                #print(para_tensor[0].grad)
+                #print(para_tensor[1].grad)
+
+
+                value_loss_epoch += value_loss.item()
+                action_loss_epoch += action_loss.item()
+                dist_entropy_epoch += dist_entropy.item()
+                kl_div_epoch += kl_div.item()
+
+        num_updates = self.ppo_epoch * self.num_mini_batch
+        value_loss_epoch /= num_updates
+        action_loss_epoch /= num_updates
+        dist_entropy_epoch /= num_updates
+        kl_div_epoch /= num_updates
+        mean_loss = value_loss_epoch * self.value_loss_coef +  action_loss_epoch
+        mean_loss += dist_entropy_epoch*self.entropy_coef if self.actor_behaviors is None else kl_div_epoch*self.entropy_coef
+        return value_loss_epoch, action_loss_epoch, dist_entropy_epoch, kl_div_epoch, mean_loss
+
+
+def ppo_rollout(num_steps, envs, actor_critic, rollouts, det=False):
+    for step in range(num_steps):
+        # Sample actions
+        with torch.no_grad():
+            value, action, action_log_prob, recurrent_hidden_states, _ = actor_critic.act(
+                rollouts.get_obs(step), rollouts.recurrent_hidden_states[step],rollouts.masks[step], deterministic = det)
+
+        # Obser reward and next obs
+#         action = action * 0
+        obs, reward, done, infos = envs.step(action)
+
+        # If done then clean the history of observations.
+        masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
+        bad_masks = torch.FloatTensor([[0.0] if 'bad_transition' in info.keys() else [1.0] for info in infos])
+
+        rollouts.insert(obs, recurrent_hidden_states, action,
+                        action_log_prob, value, reward, masks, bad_masks)
+
+
+def ppo_update(agent, actor_critic, rollouts, use_gae, gamma, gae_lambda, use_proper_time_limits, rew_func):
+    with torch.no_grad():
+        next_value = actor_critic.get_value(rollouts.get_obs(-1), rollouts.recurrent_hidden_states[-1],
+            rollouts.masks[-1]).detach()
+
+    # create 2 rollouts one for the intrinsic rewards, the other for the extrinsic rewards
+    # later we can make it more efficient, for state-input envs it makes almost no difference
+    rollouts.compute_returns(next_value, use_gae, gamma, gae_lambda, use_proper_time_limits)
+    # create intrinisc reward rollout and compute returns
+    in_rollouts = agent.change_reward(rollouts)
+    in_rollouts.compute_returns(next_value, use_gae, gamma, gae_lambda, use_proper_time_limits)
+    # the update uses both rollouts
+    value_loss, action_loss, dist_entropy, kl_div, loss = agent.update(in_rollouts, rollouts)
+    rollouts.after_update()
+    in_rollouts.after_update()
+    return value_loss, action_loss, dist_entropy, kl_div, loss
+
+
+def ppo_save_model(actor_critic, fname, iter):
+    #avoid overwrite last model for safety
+    torch.save(actor_critic.state_dict(), fname + ".tmp")
+    os.rename(fname + '.tmp', fname)
+    copy2(fname,fname+".{}".format(iter))
---
+++ ppo/model.py
@@ -0,0 +1,155 @@
+import numpy as np
+import torch
+import os
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+import gym
+
+from ppo.distributions import Bernoulli, Categorical, DiagGaussian
+from ppo.utils import init
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class MetaPolicy(nn.Module):
+    def __init__(self, input_space, action_space):
+        super(MetaPolicy, self).__init__()
+
+        obs_shape = input_space.shape
+        act_shape = action_space.shape
+
+        #######################################################################
+        #                              POLICY                                 #
+        #######################################################################
+
+        self.base = PolicyMLP(obs_shape[0])
+
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n
+            self.dist = Categorical(self.base.output_size, num_outputs)
+        elif action_space.__class__.__name__ == "Box":
+            num_outputs = action_space.shape[0]
+            self.dist = DiagGaussian(self.base.output_size, num_outputs)
+        elif action_space.__class__.__name__ == "MultiBinary":
+            num_outputs = action_space.shape[0]
+            self.dist = Bernoulli(self.base.output_size, num_outputs)
+        else:
+            raise NotImplementedError
+
+        self.policy = nn.Sequential(self.base, self.dist)
+
+        #######################################################################
+        #                             META NET                                #
+        #######################################################################
+
+        self.meta_net = MetaPolicy(obs_shape[0], act_shape[0])
+
+    @property
+    def is_recurrent(self):
+        return False
+
+    @property
+    def recurrent_hidden_state_size(self):
+        """Size of rnn_hx."""
+        return 10
+
+    def forward(self, inputs, rnn_hxs, masks):
+        raise NotImplementedError
+
+    def act(self, inputs, rnn_hxs, masks, deterministic=False):
+
+        value, actor_features, rnn_hxs = self.base(inputs)
+        dist = self.dist(actor_features)
+
+        if deterministic:
+            action = dist.mode()
+        else:
+            action = dist.sample()
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action, action_log_probs, rnn_hxs, dist_entropy
+
+    def get_value(self, inputs, rnn_hxs, masks):
+        value, _, _ = self.base(inputs)
+        return value
+
+    def evaluate_actions(self, inputs, rnn_hxs, masks, action):
+
+        value, actor_features, rnn_hxs = self.base(inputs)
+        dist = self.dist(actor_features)
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action_log_probs, dist_entropy, rnn_hxs, dist
+
+    def actions_prob(self, inputs, rnn_hxs, masks, action):
+
+        value, actor_features, rnn_hxs = self.base(inputs)
+        dist = self.dist(actor_features)
+        action_probs = dist.probs
+        return action_probs
+
+    def predict_intrinsic(self, inputs, actions):
+
+        int_rews, int_vals = self.meta_net(inputs, actions)
+
+        return int_rews, int_vals
+
+
+class PolicyMLP:
+    def __init__(self, num_inputs, hidden_size=64):
+        super(PolicyMLP, self).__init__()
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), np.sqrt(2))
+
+        self.actor = nn.Sequential(
+            init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
+
+        self.critic = nn.Sequential(
+            init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
+
+        self.critic_linear = init_(nn.Linear(hidden_size, 1))
+
+        self.train()
+
+    def forward(self, inputs, *args):
+
+        hidden_critic = self.critic(inputs)
+        hidden_actor = self.actor(inputs)
+
+        return self.critic_linear(hidden_critic), hidden_actor
+
+
+class MetaMLP:
+    def __init__(self, num_obs_inputs, num_act_inputs, hidden_size=64):
+        super(MetaMLP, self).__init__()
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), np.sqrt(2))
+
+        self.meta_reward = nn.Sequential(
+            init_(nn.Linear(num_obs_inputs + num_act_inputs, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, 1)))
+
+        self.meta_critic = nn.Sequential(
+            init_(nn.Linear(num_obs_inputs, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, 1)))
+
+        self.train()
+
+    def forward(self, inputs, actions, *args):
+
+        rews = self.meta_reward(torch.cat(inputs, actions))
+        vals = self.meta_critic(inputs)
+
+        return rews, vals
